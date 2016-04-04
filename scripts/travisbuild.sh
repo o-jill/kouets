9d812d32c2c0b44d4c2066f373047fb14c179ff2
o-jill <nobaokix@gmail.com>
2016-04-04 10:52:54 +0900

    change travis build scripts to separate normal build an test.

---
+++ scripts/travisbuild.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# a build script for travis ci
+
+if [ "${TRAVIS_BUILD_TYPE}" = "test" ]; then
+    echo "let us TEST !!"
+    cd src/test
+    qmake -qt=qt4 ./test.pro
+#    make clean
+#    make mocclean
+    make
+    mkdir debug
+    cp test debug/test
+    ./debug/test
+else
+    echo "normal  build"
+    qmake -qt=qt4 -v ./src/kouets.pro
+    ./src/gen_git_hash.sh H kouetshash
+    ./src/gen_git_hash.sh CPP kouetshash
+    qmake -qt=qt4 ./src/kouets.pro
+    make
+fi
