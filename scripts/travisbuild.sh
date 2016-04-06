e121216e6157ad15d6c174fa693a4683f696dca1
o-jill <nobaokix@gmail.com>
2016-04-06 11:46:44 +0900

    add clang build for normal build and test.

--- scripts/travisbuild.sh
+++ scripts/travisbuild.sh
@@ -19,8 +19,31 @@ if [ "${TRAVIS_BUILD_TYPE}" = "test" ]; then
         echo "some tests failed ... (${?})"
     fi
     exit ${exitcode}
+elif [ "${TRAVIS_BUILD_TYPE}" = "testclang" ]; then
+    echo "let us TEST with clang!!"
+    cd src/test
+    qmake -qt=qt4 ./test.pro -spec unsupported/linux-clang
+#    qmake -qt=qt4 ./test.pro -spec linux-llvm
+    make
+    mkdir debug
+    cp test debug/test
+    ./debug/test
+    exitcode=$?
+    if [ ${exitcode} = 0 ]; then
+        echo "ALL tests finished successfully!! (${?})"
+    else
+        echo "some tests failed ... (${?})"
+    fi
+    exit ${exitcode}
+elif [ "${TRAVIS_BUILD_TYPE}" = "clang" ]; then
+    echo "clang build"
+    qmake -qt=qt4 -v ./src/kouets.pro -spec unsupported/linux-clang
+    ./src/gen_git_hash.sh H kouetshash
+    ./src/gen_git_hash.sh CPP kouetshash
+    qmake -qt=qt4 ./src/kouets.pro -spec unsupported/linux-clang
+    make
 else
-    echo "normal  build"
+    echo "gcc build"
     qmake -qt=qt4 -v ./src/kouets.pro
     ./src/gen_git_hash.sh H kouetshash
     ./src/gen_git_hash.sh CPP kouetshash
